947f8d697a531d5bf56aac5b503b824bc6ef531c
Petrified0110 <petra.horvath99@gmail.com>
2021-11-21 17:32:04 +0200

    Consume and deserialize data from kafka topic

--- ScoreboardProject/src/main/java/kafka/KafkaConsumerUtils.java
+++ ScoreboardProject/src/main/java/kafka/KafkaConsumerUtils.java
@@ -1,5 +1,7 @@
 package kafka;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import domain.InputData;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -7,6 +9,7 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Properties;
 
@@ -34,14 +37,10 @@ public class KafkaConsumerUtils {
 
     private static Consumer<Long, String> createConsumer() {
         final Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                IKafkaConstants.BOOTSTRAP_SERVERS);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG,
-                "KafkaExampleConsumer");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                LongDeserializer.class.getName());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class.getName());
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, IKafkaConstants.BOOTSTRAP_SERVERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaExampleConsumer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 
 
         Consumer<Long, String> consumer = new KafkaConsumer<>(props);
@@ -54,6 +53,7 @@ public class KafkaConsumerUtils {
         final Consumer<Long, String> consumer = createConsumer();
 
         final int giveUp = 100;   int noRecordsCount = 0;
+        ObjectMapper objectMapper = new ObjectMapper();
 
         while (true) {
             final ConsumerRecords<Long, String> consumerRecords =
@@ -66,9 +66,15 @@ public class KafkaConsumerUtils {
             }
 
             consumerRecords.forEach(record -> {
-                System.out.printf("Consumer Record:(%d, %s, %d, %d)\n",
-                        record.key(), record.value(),
-                        record.partition(), record.offset());
+                try {
+                    InputData data = objectMapper.readValue(record.value(), InputData.class);
+
+                    System.out.print(data.playerId + " " + data.boxesDestroyed + " " + data.playerLivesTaken);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
             });
 
             consumer.commitAsync();
