d305c8b11944d784f7b0d51057360f89d62e0703
Petrified0110 <petra.horvath99@gmail.com>
2021-11-28 18:28:22 +0200

    Write kafka consumers for game initializer and ongoing received data and producer for processed data

---
+++ ScoreboardProject/src/main/java/kafka/KafkaConsumerGameInitializer.java
@@ -0,0 +1,49 @@
+package kafka;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import domain.GameStartData;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class KafkaConsumerGameInitializer extends BaseKafkaConsumer{
+
+    public KafkaConsumerGameInitializer() {
+        super("game-initializer-topic");
+    }
+
+    public GameStartData runConsumer() {
+        final Consumer<Long, String> consumer = createConsumer();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ArrayList<GameStartData> gameStartData = new ArrayList<GameStartData>();
+
+        while (true) {
+            final ConsumerRecords<Long, String> consumerRecords =
+                    consumer.poll(1000);
+
+            consumerRecords.forEach(record -> {
+                GameStartData data;
+                try {
+                    data = objectMapper.readValue(record.value(), GameStartData.class);
+
+                    System.out.print(data.numberOfPlayers + " " + data.livesPerPlayer + " " + data.numberOfBoxes + " " + data.pointsForBox + " " + data.pointsForLife);
+
+                    gameStartData.add(data);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            consumer.commitAsync();
+
+            if(!gameStartData.isEmpty()){
+                consumer.close();
+                System.out.println("Game initializer: retrieved initialization data.\nClosing consumer");
+                return gameStartData.get(0);
+            }
+        }
+    }
+}
